4c631968333364413327499d42c2b50577e3d756
Kai Ma <k78ma@uwaterloo.ca>
2024-08-30 20:11:12 -0600

    task 0.1

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -34,6 +34,125 @@ from typing import Callable, Iterable
 
 # TODO: Implement for Task 0.1.
 
+def mul(x: float, y: float) -> float:
+    """
+    Multiplies two numbers.
+    """
+    return x * y
+
+
+def id(x: float) -> float:
+    """
+    Returns the input unchanged.
+    """
+    return x
+
+
+def add(x: float, y: float) -> float:
+    """
+    Adds two numbers.
+    """
+    return x + y
+
+
+def neg(x: float) -> float:
+    """
+    Negates a number.
+    """
+    return -x
+
+
+def lt(x: float, y: float) -> bool:
+    """
+    Checks if one number is less than another.
+    """
+    return x < y
+
+
+def eq(x: float, y: float) -> bool:
+    """
+    Checks if two numbers are equal.
+    """
+    return x == y
+
+
+def max(x: float, y: float) -> float:
+    """
+    Returns the larger of two numbers.
+    """
+    if x > y:
+        return x
+    else:
+        return y
+
+
+def is_close(x: float, y: float) -> bool:
+    """
+    Checks if two numbers are close in value.
+    """
+    return abs(x - y) < 1e-2
+
+
+def sigmoid(x: float) -> float:
+    """
+    Calculates the sigmoid function.
+    """
+    if x >= 0:
+        return 1.0 / (1.0 + math.exp(-x))
+    else:
+        return math.exp(-x) / (1.0 + math.exp(-x))
+
+
+def relu(x: float) -> float:
+    """
+    Applies the ReLU activation function
+    """
+    return max(0.0, x)
+
+
+def log(x: float) -> float:
+    """
+    Calculates the natural logarithm.
+    """
+    return math.ln(x)
+
+
+def exp(x: float) -> float:
+    """
+    Calculates the exponential function.
+    """
+    return math.exp(x)
+
+
+def inv(x: float) -> float:
+    """
+    Calculates the reciprocal
+    """
+    return 1.0 / x
+
+
+def log_back(x: float, b: float) -> float:
+    """
+    Computes the derivative of log times a second arg
+    """
+    return (1.0 / x) * b
+
+
+def inv_back(x: float, b: float) -> float:
+    """
+    Computes the derivative of reciprocal times a second arg
+    """
+    return (-1.0 / math.pow(x, 2)) * b
+
+
+def relu_back(x: float, b: float) -> float:
+    if x >= 0:
+        return 1.0 * b
+    else:
+        return 0.0
+
+
+
 
 # ## Task 0.3
 
@@ -52,3 +171,14 @@ from typing import Callable, Iterable
 
 
 # TODO: Implement for Task 0.3.
+def negList():
+    return NotImplemented
+
+def addLists():
+    return NotImplemented
+
+def sum():
+    return NotImplemented
+
+def prod():
+    return NotImplemented
