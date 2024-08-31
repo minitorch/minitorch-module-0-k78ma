10bf0ade58f00522fad496ddef70b2c5c7e52c95
Kai Ma <k78ma@uwaterloo.ca>
2024-08-31 12:59:07 -0600

    task 0.2

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -3,7 +3,6 @@
 import math
 
 # ## Task 0.1
-from typing import Callable, Iterable
 
 #
 # Implementation of a prelude of elementary functions.
@@ -34,52 +33,39 @@ from typing import Callable, Iterable
 
 # TODO: Implement for Task 0.1.
 
+
 def mul(x: float, y: float) -> float:
-    """
-    Multiplies two numbers.
-    """
+    """Multiplies two numbers."""
     return x * y
 
 
 def id(x: float) -> float:
-    """
-    Returns the input unchanged.
-    """
+    """Returns the input unchanged."""
     return x
 
 
 def add(x: float, y: float) -> float:
-    """
-    Adds two numbers.
-    """
+    """Adds two numbers."""
     return x + y
 
 
 def neg(x: float) -> float:
-    """
-    Negates a number.
-    """
+    """Negates a number."""
     return -x
 
 
 def lt(x: float, y: float) -> bool:
-    """
-    Checks if one number is less than another.
-    """
+    """Checks if one number is less than another."""
     return x < y
 
 
 def eq(x: float, y: float) -> bool:
-    """
-    Checks if two numbers are equal.
-    """
+    """Checks if two numbers are equal."""
     return x == y
 
 
 def max(x: float, y: float) -> float:
-    """
-    Returns the larger of two numbers.
-    """
+    """Returns the larger of two numbers."""
     if x > y:
         return x
     else:
@@ -87,71 +73,55 @@ def max(x: float, y: float) -> float:
 
 
 def is_close(x: float, y: float) -> bool:
-    """
-    Checks if two numbers are close in value.
-    """
-    return abs(x - y) < 1e-2
+    """Checks if two numbers are close in value."""
+    return abs(x - y) < 1e-1
 
 
 def sigmoid(x: float) -> float:
-    """
-    Calculates the sigmoid function.
-    """
+    """Calculates the sigmoid function."""
+    # $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
     if x >= 0:
         return 1.0 / (1.0 + math.exp(-x))
     else:
-        return math.exp(-x) / (1.0 + math.exp(-x))
+        return math.exp(x) / (1.0 + math.exp(x))
 
 
 def relu(x: float) -> float:
-    """
-    Applies the ReLU activation function
-    """
+    """Applies the ReLU activation function"""
     return max(0.0, x)
 
 
 def log(x: float) -> float:
-    """
-    Calculates the natural logarithm.
-    """
+    """Calculates the natural logarithm."""
     return math.ln(x)
 
 
 def exp(x: float) -> float:
-    """
-    Calculates the exponential function.
-    """
+    """Calculates the exponential function."""
     return math.exp(x)
 
 
 def inv(x: float) -> float:
-    """
-    Calculates the reciprocal
-    """
+    """Calculates the reciprocal."""
     return 1.0 / x
 
 
 def log_back(x: float, b: float) -> float:
-    """
-    Computes the derivative of log times a second arg
-    """
+    """Computes the derivative of log times a second arg."""
     return (1.0 / x) * b
 
 
 def inv_back(x: float, b: float) -> float:
-    """
-    Computes the derivative of reciprocal times a second arg
-    """
+    """Computes the derivative of reciprocal times a second arg."""
     return (-1.0 / math.pow(x, 2)) * b
 
 
 def relu_back(x: float, b: float) -> float:
+    """Computes the derivative of ReLU times a second arg."""
     if x >= 0:
         return 1.0 * b
     else:
         return 0.0
-
-
 
 
 # ## Task 0.3
@@ -174,11 +144,14 @@ def relu_back(x: float, b: float) -> float:
 def negList():
     return NotImplemented
 
+
 def addLists():
     return NotImplemented
 
+
 def sum():
     return NotImplemented
+
 
 def prod():
     return NotImplemented
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -107,41 +107,65 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+
+    # Check if sigmoid is always between 0.0 and 1.0
+    assert 0.0 <= sigmoid(a) <= 1.0
+
+    # Check if one minus sigmoid is the same as sigmoid of the negative
+    assert_close(1 - sigmoid(a), sigmoid(-a))
+
+    # Check if it crosses 0 at 0.5
+    assert_close(sigmoid(0.0), 0.5)
+
+    # Check if it is strictly increasing
+    if a < 0:
+        assert sigmoid(a) <= sigmoid(0)
+    elif a > 0:
+        assert sigmoid(0) <= sigmoid(a)
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
 
+    # a < b and b < c implies a < c
+    if lt(a, b) and lt(b, c):
+        assert lt(a, c) == 1.0
+    
+    # a = b and b < c implies a < c
+    if eq(a, b) == 1.0 and lt(b, c) == 1.0:
+        assert lt(a, c) == 1.0
+    
+    # a < b and b = c implies a < c
+    if lt(a, b) == 1.0 and eq(b, c) == 1.0:
+        assert lt(a, c) == 1.0
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+@given(small_floats, small_floats)
+def test_symmetric(a: float, b: float) -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
-
+    assert_close(a*b, b*a)
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+@given(small_floats, small_floats, small_floats)
+def test_distribute(a: float, b: float, c: float) -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(a*(b+c), (a*b)+(a*c))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+@given(small_floats, small_floats)
+def test_other(a: float, b: float) -> None:
     """Write a test that ensures some other property holds for your functions."""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+
+    # a-b = a+(-b)
+    
+    assert_close(a-b, add(a, neg(b)))
 
 
 # ## Task 0.3  - Higher-order functions
